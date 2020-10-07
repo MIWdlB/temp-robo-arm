896a0eba426975a8ee4160caaa25673bf1056620
Michael Williams <ucapiwi@ucl.ac.uk>
2020-10-07 12:38:27 +0100

    add bone class cause why not

--- arm.py
+++ arm.py
@@ -3,6 +3,7 @@ Robot arm class
 '''
 
 from joint import Joint
+from bone import Bone
 from pincer import Pincer
 
 from typing import Optional, List, Union, Tuple
@@ -13,20 +14,21 @@ class Arm():
 
     should I add a "Bone" class? Depends how we implement the animation I think
     """
-    def __init__(self, parts: Optional[List[Union[Joint, float, Pincer]]] = []) -> None:
+    def __init__(self, parts: Optional[List[Union[Joint, Bone, Pincer]]] = []) -> None:
         self.parts = parts
 
     def add_joint(self, joint: Joint) -> None:
         self.parts.append(joint)
 
-    def add_bone(self, bone: float) -> None:
+    def add_bone(self, bone: Bone) -> None:
         self.parts.append(bone)
 
-    @property
-    def end_points(self) -> List[Tuple[Tuple[float, float, float], Tuple[float, float, float]]]:
-        start_position = (0,0,0)
-        for part in self.parts:
-            pass
+    def show_parts(self):
+        for index, part in enumerate(self.parts):
+            print(f'Part No: {index}\tType: {type(part)}')
+
+    def add_instruction(self, part_name: str, movement: float) -> None:
+        pass
 
 
 
---
+++ bone.py
@@ -0,0 +1,7 @@
+"""
+Defines a class for rigid sections
+"""
+
+class Bone():
+    def __init__(self, length: float) -> None:
+        self.length = length
